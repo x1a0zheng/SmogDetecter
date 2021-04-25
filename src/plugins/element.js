e8550d25c27d86cd79ead936476ccaec11ad926e
Rayxuln <imbosslx@gmail.com>
2021-04-25 16:16:06 +0800

    Add ElementUI-Plus plugin

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import { ElButton, ElMessage } from 'element-plus'
+import lang from 'element-plus/lib/locale/lang/zh-cn'
+import locale from 'element-plus/lib/locale'
+
+export default (app) => {
+  locale.use(lang)
+  app.use(ElButton)
+  app.use(ElMessage)
+}
