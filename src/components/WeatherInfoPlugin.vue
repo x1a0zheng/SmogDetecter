d27dc02be448cfe425a980a234bfeefab969030a
xz <xz_2018_xd@163.com>
2021-04-26 19:10:04 +0800

    feature: fix WeatherInfo page style

--- src/components/WeatherInfoPlugin.vue
+++ src/components/WeatherInfoPlugin.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="weather-info-containor" v-if="weatherData">
     <div class="weather-info-item" v-for="(value, name) in index" v-bind:key="name">
-      <div class="center">{{value}} : </div>
-      <div class="center">{{weatherData[name]}}</div>
+      <div class="item-containor">
+        <div class="center">{{value}} :</div>
+        <div class="center"> {{weatherData[name]}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,14 +36,25 @@ export default {
 .weather-info-containor {
   display: flex;
   flex-wrap: wrap;
+  align-content: flex-start;
+}
+.item-containor {
+  display: flex;
+  justify-content: center;
+  line-height: 50px;
+  width: 150px;
+  margin: 0 auto;
+  color: white;
+  background: #fc4a1a;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, rgb(252, 74, 26), rgb(247, 183, 51));  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, rgb(252, 74, 26), rgb(247, 183, 51)); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  border-radius: 15px;
+  font-weight: bold;
 }
 .weather-info-item {
   flex-basis: 25%;
   height: 50px;
-  display: flex;
-  justify-content: center;
-}
-.center {
-  line-height: 50px;
+  min-width: 180px;
+  margin: 10px 0;
 }
 </style>
--- src/views/WeatherInfo.vue
+++ src/views/WeatherInfo.vue
@@ -1,5 +1,9 @@
 <template >
-  <div>
+  <div class="containor">
+    <div class="title">
+      <button class="back-btn" @click="returnHome">返回</button>
+      <h2 class="title-text">未来3天天气变化</h2>
+    </div>
     <div id="weatherGraph" ref="weatherGraph" :style="{width : '100%', height: '60vh'}"></div>
     <weather-info-plugin class="plugin" v-bind:weatherData="info"></weather-info-plugin>
   </div>
@@ -24,6 +28,9 @@ export default {
     'weather-info-plugin': WeatherInfoPlugin
   },
   methods: {
+    returnHome () {
+      this.$router.push('/')
+    },
     getWeatherData () {
       console.log(this.$store.state.key, this.$store.state.location)
       const pos = this.$store.state.location
@@ -44,18 +51,16 @@ export default {
     req.then(res => {
       console.log(res)
       this.infoData = res.daily
+      this.info = this.infoData[0]
       const maxTemp = []; const minTemp = []
       for (const item of res.daily) {
         maxTemp.push(parseInt(item.tempMax))
         minTemp.push(parseInt(item.tempMin))
       }
-      // const weatherGraph = this.$refs.weatherGraph
-      const weatherGraph = document.getElementById('weatherGraph')
+      const weatherGraph = this.$refs.weatherGraph
+      // const weatherGraph = document.getElementById('weatherGraph')
       const chart = echarts.init(weatherGraph)
       const option = {
-        title: {
-          text: '未来3天气温变化'
-        },
         tooltip: {
           trigger: 'axis'
         },
@@ -110,7 +115,25 @@ export default {
 </script>
 
 <style scoped>
+.containor {
+  overflow: auto;
+}
 .plugin {
-  margin-top: 50px;
+  height: 30vh;
+  overflow: auto;
+}
+.title {
+  height: 10vh;
+  text-align: center;
+  display: flow-root;
+}
+.back-btn {
+  float: left;
+  margin-left: 1rem;
+  margin-top: 20px;
+  height: 30px;
+  background-color: #0876e4;
+  color: white;
+  border-radius: 3px;
 }
 </style>
